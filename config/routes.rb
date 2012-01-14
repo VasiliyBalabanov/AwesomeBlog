215324e419a2808a6c1b08f21ac1bab4dd17087c
Vasiliy Balabanov <moon-imba@yandex.ru>
2012-01-14 04:04:49 +0400

    Another Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "users#index"
   resources :users do
     resources :posts do
       resources :comments
