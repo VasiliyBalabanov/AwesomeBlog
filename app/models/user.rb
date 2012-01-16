0e7e9d804de55d64f3b468ccf61a3ae174fb9b45
Vasiliy Balabanov <moon-imba@yandex.ru>
2012-01-16 12:11:12 +0400

    Minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
-  has_many :posts
-  has_many :comments
+  has_many :posts, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   
   attr_accessor :password
   before_save :encrypt_password
