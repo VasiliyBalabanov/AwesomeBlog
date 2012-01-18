a8265c1f7a8335b119bc8077d181a4072fd474b1
Vasiliy Balabanov <moon-imba@yandex.ru>
2012-01-18 16:06:17 +0400

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Blog::Application.configure do
-  config.action_mailer.default_url_options = { host: "localhost:3000"}
+  config.action_mailer.default_url_options = { host: "http://ec2-107-20-197-0.compute-1.amazonaws.com/"}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
