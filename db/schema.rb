2cf26a8af497bdefaf282ae3769a96f7b437c191
Vasiliy Balabanov <moon-imba@yandex.ru>
2012-01-26 01:41:43 +0400

    Minor changes and is_private field added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120121145740) do
+ActiveRecord::Schema.define(:version => 20120125211743) do
 
   create_table "comments", :force => true do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20120121145740) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_private", :default => false
   end
 
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
